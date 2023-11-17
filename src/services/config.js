1db4e3b3c1c03ed9cd5c076cefa2da105c4aad57
Facu Ramirez <facuramirez937@gmail.com>
2023-11-17 19:34:25 -0300

    actualizacion de archivo config

--- src/services/config.js
+++ src/services/config.js
@@ -1,12 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
-// Vamos a importar dos funciones de Firebase
 
-// initializeApp: Esta funcion la utilizo para Iniciar la conexion con Firebase.
-// getFirestore: Esta funcion la utilizo para obtener una instancia de Firestore.
-
-
-// Estamos trabajando con un objeto con toda la informacion de la cuenta. Aca se incluye la "key" personal de acceso a la Base de Datos (BD)
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "tienda-guitarras-facu.firebaseapp.com",
@@ -16,7 +10,6 @@ const firebaseConfig = {
   appId: "1:463603827766:web:dbaa8b6099a0b584b60497"
 };
 
-// Inicializamos Firebase, y se pasa la configuracion como argumento. Esto retorna una instancia de Firebase.
+
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-// Exportamos esta referencia para que este disponible en toda nuestra aplicacion.
